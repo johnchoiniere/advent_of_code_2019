7fdf73c6550b8b13a601b0e857e8c39e137b7705
john <john.choiniere@gmail.com>
2019-12-04 00:05:13 -0800

    added comments to run-through script

--- day_4/day4_straight_through.py
+++ day_4/day4_straight_through.py
@@ -5,12 +5,15 @@ Created on Tue Dec  3 23:29:57 2019
 
 @author: john
 """
-
+# My range values
 start = 153517
 end = 630395
 
+# Empty list to append matching pwds to
 matches = []
 
+# Loop through the range, use the logic that the minimum of the set of
+# sequential differences between the numbers must be zero to qualify
 for x in range(start, end):
     n = [int(i) for i in [char for char in str(x)]]
     diffs = []
@@ -19,11 +22,13 @@ for x in range(start, end):
     if min(diffs)==0:
         matches.append(x)
 
-
+# Print the part 1 answer
 print("Part 1 answer: "+str(len(matches)))
 
+# Empty list to append P2 matching pwds to
 p2matches = []
 
+# For the matches, apply the additional criterion
 for x in matches:
     n = [int(i) for i in [char for char in str(x)]]
     if n[0] == n[1] and n[0] != n[2]:
@@ -37,5 +42,5 @@ for x in matches:
     elif n[4] == n[5] and n[4] != n[3]:
         p2matches.append(x)
 
-
+# Print the next part answer
 print("Part 2 answer: "+str(len(p2matches)))
