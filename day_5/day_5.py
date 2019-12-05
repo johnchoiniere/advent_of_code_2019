45fbe736b9995d5c775e1b00f840e742f2748b47
John Choiniere <jchoiniere@mariners.com>
2019-12-05 10:53:35 -0800

    ugly, many-if-elif-else, uncommented version done; will expand/clean later

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -7,7 +7,7 @@ Created on Mon Dec  2 11:06:47 2019
 
 # read in the data; I'm... not great at doing I/O in python, tbh
 with open('input.txt') as f:
-    infile = f.readlines()
+    infile = f.readlines()[0].strip().split(',')
 
 '''
 Retrospectively I made it a function rather than loose code. That should make it more reusable, too.
@@ -23,10 +23,13 @@ def opcode_d5(input_val, l):
     end = False
     pointer = 0
     while not end:
+        #time.sleep(0.3333333)
         instruct = str(l[pointer]).rjust(5,'0')
         operation = instruct[len(instruct)-2:]
         a_mode = int(instruct[2])
         b_mode = int(instruct[1])
+        
+        #print("Pointer: "+str(pointer)+" --- Op code: "+operation)
         
         if operation == '99':
             end = True
@@ -37,14 +40,14 @@ def opcode_d5(input_val, l):
             elif a_mode == 1:
                 a_val = l[pointer+1]
             else:
-                print("ERROR IN ASSIGNING A_VAL")
+                print("INST 01 ERROR IN ASSIGNING A_VAL")
                 break
             if b_mode == 0:
                 b_val = l[l[pointer+2]]
             elif b_mode == 1:
                 b_val = l[pointer+2]
             else:
-                print("ERROR IN ASSIGNING B_VAL")
+                print("INST 01 ERROR IN ASSIGNING B_VAL")
                 break
             l[l[pointer+3]] = a_val + b_val
             pointer += 4
@@ -55,14 +58,14 @@ def opcode_d5(input_val, l):
             elif a_mode == 1:
                 a_val = l[pointer+1]
             else:
-                print("ERROR IN ASSIGNING A_VAL")
+                print("INST 02 ERROR IN ASSIGNING A_VAL")
                 break
             if b_mode == 0:
                 b_val = l[l[pointer+2]]
             elif b_mode == 1:
                 b_val = l[pointer+2]
             else:
-                print("ERROR IN ASSIGNING B_VAL")
+                print("INST 02 ERROR IN ASSIGNING B_VAL")
                 break
             l[l[pointer+3]] = a_val * b_val
             pointer += 4
@@ -80,20 +83,81 @@ def opcode_d5(input_val, l):
                 a_val = l[l[pointer+1]]
             elif a_mode == 1:
                 a_val = l[pointer+1]
-            if a_val != 0:
-                if b_mode == 0:
-                    pointer = l[l[pointer+2]]
-                elif b_more == 1:
-                    pointer = l[pointer+2]
-                else:
-                    print("INST 05 ERROR INVALID MODE")
-                    break
             else:
-                 pointer += 1   
+                print("INST 05 ERROR A_MODE INVALID")
+                break
+            if b_mode == 0:
+                b_val = l[l[pointer+2]]
+            elif b_mode == 1:
+                b_val = l[pointer+2]
+            else:
+                print("INST 05 ERROR B_MODE INVALID")
+                break
+            if a_val != 0:
+                pointer = b_val
+            else:
+                 pointer += 3
                 
         elif operation == '06':
             if a_mode == 0:
                 a_val = l[l[pointer+1]]
-                
+            elif a_mode == 1:
+                a_val = l[pointer+1]
+            else:
+                print("INST 06 ERROR A_MODE INVALID")
+                break
+            if b_mode == 0:
+                b_val = l[l[pointer+2]]
+            elif b_mode == 1:
+                b_val = l[pointer+2]
+            else:
+                print("INST 06 ERROR B_MODE INVALID")
+                break
+            if a_val == 0:
+                pointer = b_val
+            else:
+                pointer += 3
+                        
+        elif operation == '07':
+            if a_mode == 0:
+                a_val = l[l[pointer+1]]
+            elif a_mode == 1:
+                a_val = l[pointer+1]
+            else:
+                print("INST 07 ERROR A_MODE INVALID")
+                break
+            if b_mode == 0:
+                b_val = l[l[pointer+2]]
+            elif b_mode == 1:
+                b_val = l[pointer+2]
+            else:
+                print("INST 07 ERROR B_MODE INVALID")
+                break
+            if a_val < b_val:
+                l[l[pointer+3]] = 1
+            else:
+                l[l[pointer+3]] = 0
+            pointer += 4
+            
+        elif operation == '08':
+            if a_mode == 0:
+                a_val = l[l[pointer+1]]
+            elif a_mode == 1:
+                a_val = l[pointer+1]
+            else:
+                print("INST 08 ERROR A_MODE INVALID")
+                break
+            if b_mode == 0:
+                b_val = l[l[pointer+2]]
+            elif b_mode == 1:
+                b_val = l[pointer+2]
+            else:
+                print("INST 08 ERROR B_MODE INVALID")
+                break
+            if a_val == b_val:
+                l[l[pointer+3]] = 1
+            else:
+                l[l[pointer+3]] = 0
+            pointer += 4
             
     return ret
